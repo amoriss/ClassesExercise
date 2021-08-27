c5cce3cfa68c898693af738d54fed2084cb85e4a
Amoriss <amoriss@protonmail.com>
2021-08-27 14:00:10 -0500

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    class Car
+    {
+        /* Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public */ 
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+
+
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var firstCar = new Car();
+            //Car firstCar = new Car(); //same as above
+            firstCar.Make = "Toyota";
+            firstCar.Model = "Corolla";
+            firstCar.Year = 2003;
+            Console.WriteLine($"Make: {firstCar.Make}. Model: {firstCar.Model}. Year:{firstCar.Year}");
+        }
+    }
+}
