5140d5cce148d26c271d4d19006f4d97857a2742
Amoriss <amoriss@protonmail.com>
2021-08-29 12:42:39 -0500

    edited code

--- ClassesExercise/Car.cs
+++ ClassesExercise/Car.cs
@@ -7,13 +7,35 @@ using System.Threading.Tasks;
 namespace ClassesExercise
 {
     class Car
+           
     {
-        /* Create a Make property of type string that is public
-Create a Model property of type string that is public
-Create a Year property of type int that is public */ 
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            //Console.WriteLine($"Make: {Make}. Model: { Model} . Year {Year}");
+            PrintCars();
+        }
+
+        public void PrintCars()
+        {
+            Console.WriteLine($"Make: {Make}. Model: { Model} . Year {Year}");
+        }
+
+        /* Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public */ 
+     
 
 
 
--- ClassesExercise/Program.cs
+++ ClassesExercise/Program.cs
@@ -6,13 +6,26 @@ namespace ClassesExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Classes Exercise:");
             var firstCar = new Car();
             //Car firstCar = new Car(); //same as above
             firstCar.Make = "Toyota";
             firstCar.Model = "Corolla";
             firstCar.Year = 2003;
-            Console.WriteLine($"Make: {firstCar.Make}. Model: {firstCar.Model}. Year:{firstCar.Year}");
+
+            var secondCar = new Car()
+            {
+                Make = "Mazda",
+                Model = "xyz203",
+                Year = 2030
+            };
+
+            firstCar.PrintCars();
+            secondCar.PrintCars();
+
+            var chevy = new Car("Chevy", "Truck", 2010);
+            //Console.WriteLine($"chevy")
+
         }
     }
 }
